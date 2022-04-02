9573a8819275ee552b5630224990df002cf6533c
Paulo da Silva <kashpaulo@gmail.com>
2022-04-02 19:05:49 -0300

    Sorteio

--- Pessoal/Sorteio/script.js
+++ Pessoal/Sorteio/script.js
@@ -13,15 +13,15 @@ function adicionar(){
 function sortear(){
   let min = 0;
   let max = pessoas.length;
-  let escolha = Math.floor(Math.random() * (max - min)+ min);
-  let resultado = pessoas[escolha]
-  document.getElementById("vencedor").innerHTML = resultado;
-}
-function limpar(){
   if(pessoas == 0){
     alert("por favor, adicione algo a lista!")
   }else{
+  let escolha = Math.floor(Math.random() * (max - min)+ min);
+  let resultado = pessoas[escolha]
+  document.getElementById("vencedor").innerHTML = resultado;
+  }
+}
+function limpar(){
     pessoas = [];
     document.getElementById("visual").innerHTML = pessoas;
-  }
 }
