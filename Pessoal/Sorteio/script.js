b647254b5d8cdccd6b38b5e1048b00741a9c6416
Paulo da Silva <kashpaulo@gmail.com>
2022-04-02 17:19:10 -0300

    Site de sorteio

---
+++ Pessoal/Sorteio/script.js
@@ -0,0 +1,23 @@
+let pessoas = [];
+function adicionar(){
+  let addPessoa = document.getElementById("nome").value;
+  if(addPessoa != ""){
+    pessoas.push(addPessoa);
+  let mostrar = document.getElementById("visual").innerHTML = pessoas.join(", ");
+  document.getElementById("nome").value = "";
+  }
+  else{
+    alert("por favor preencha o campo!")
+  }
+}
+function sortear(){
+  let min = 0;
+  let max = pessoas.length;
+  let escolha = Math.floor(Math.random() * (max - min)+ min);
+  let resultado = pessoas[escolha]
+  document.getElementById("vencedor").innerHTML = resultado;
+}
+function limpar(){
+  pessoas = [];
+  document.getElementById("visual").innerHTML = pessoas;
+}
