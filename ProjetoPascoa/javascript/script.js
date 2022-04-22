200f01f3fa57dca412df10c545de00af43fe90ac
Paulo da Silva <kashpaulo@gmail.com>
2022-04-22 02:57:00 -0300

    ProjetoPascoa

--- ProjetoPascoa/javascript/script.js
+++ ProjetoPascoa/javascript/script.js
@@ -15,7 +15,7 @@ function replicar(){
         let id = i;
         let produto = document.querySelector(".produto").cloneNode(true);
         //item
-        produto.querySelector(".tituloProduto").innerHTML = p.nome;
+        produto.querySelector(".tituloProduto").innerHTML = p.nome.toUpperCase();
         produto.querySelector(".produtoImg").src = p.img;
         produto.querySelector(".descricaoProduto").innerHTML = p.descricao;
         produto.querySelector(".precoProduto").innerHTML = p.valor;
@@ -40,3 +40,43 @@ function alterarQt(id, quantidade){
 }
 
 buscarListaProduto();
+
+let msgModal = "";
+function mostrarPedidos(){
+    let msgModalPadrao = "<p>Nenhum produto foi selecionado.</p>";
+    let subTotal = 0;
+    let total = 0;
+    for (const produto of lista) {
+        if(produto.quantidade > 0){
+            subTotal = (produto.valor * produto.quantidade).toFixed(2);
+            total += +subTotal;
+            msgModal += `<p>${produto.nome.toUpperCase()} (R$: ${produto.valor} x ${produto.quantidade}) = ${subTotal}</p>`;
+        }
+    }
+    if(msgModal == ""){
+        
+        document.querySelector("#btEnviar").disabled = "disabled"; 
+        document.querySelector(".modal-body").innerHTML = msgModalPadrao;
+    }else{
+        msgModal += `<b>Total ${total.toFixed(2)}</b>`
+        document.querySelector("#btEnviar").disabled = "";
+        document.querySelector(".modal-body").innerHTML = msgModal;
+    }
+    
+}
+
+function enviar(){
+    let fone = '992015114';
+    msgModal = msgModal.replaceAll("<p>", "").replaceAll("</p>", "\n");
+    msgModal = msgModal.replaceAll("<b>", "*").replaceAll("</b>", "*");
+    msgModal = msgModal.replaceAll("%", "");
+    let nome = document.querySelector("#nome").value;
+    let endereco = document.querySelector("#endereco").value;
+    msgModal += `\nNome: *${nome}*`
+    msgModal += `\nEndereço: *${endereco}*`
+    msgModal = encodeURI(msgModal);
+    
+    link = `https://api.whatsapp.com/send?phone=${fone}&text=${msgModal}`;
+    window.open(link, '_blanck')
+}
+
